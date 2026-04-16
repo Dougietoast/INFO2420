e4cb18afb9baf25b334259ab585a17b0877194fa
Dougietoast <jrcasa23@gmail.com>
2026-04-15 18:05:31 -0600

    project 7

---
+++ projects/Project7/myscripts.js
@@ -0,0 +1,54 @@
+PrintButton1 = new Image
+	PrintButton2 = new Image
+	logo1 = new Image
+	logo2 = new Image
+if (document.images) {
+	PrintButton1.src = "Images/Print1.jpg"
+	PrintButton2.src = "Images/Print2.jpg"
+	logo1.src = "Images/Logo1.jpg"	
+	logo2.src = "Images/Logo2.jpg"
+}
+
+
+		var imgArray = new Array(
+			'Image1Lg.jpg',
+			'Image2Lg.jpg',
+			'Image3Lg.jpg',
+			'Image4Lg.jpg'
+			
+
+		);
+
+
+		var titleArray = new Array(
+			'Dog Performing a Trick',
+			'Cat and Dog',
+			'Happy Cat',
+			'German Shepherd'
+			
+		);
+			
+		var imgPath = "Images/fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
